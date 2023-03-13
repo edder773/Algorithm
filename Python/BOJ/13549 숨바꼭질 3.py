55d2a53527181ba4c7faec65984ca0e7e1ad6e1d
edder773 <st02045@naver.com>
2023-03-13 17:54:42 +0900

    upload

---
+++ Python/BOJ/13549 숨바꼭질 3.py
@@ -0,0 +1,21 @@
+import sys
+from collections import deque
+N, K= map(int, sys.stdin.readline().split())
+queue = deque()
+queue.append(N)
+way = [0]*100001 # 최대 크기
+while queue:
+    a =  queue.popleft()
+    if a == K: # 둘이 만났을 때
+        result = way[a]
+        break
+    
+    for i in [a*2, a-1, a+1]:
+        if 0 <= i < 100001 and way[i] == 0:
+            if i == a*2 :
+                way[i] = way[a]
+                queue.append(i) 
+            else : 
+                way[i] = way[a] + 1
+                queue.append(i)
+print(result)
