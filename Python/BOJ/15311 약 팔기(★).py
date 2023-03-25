cba031927ff1e41ef5cc4df51b52fece182d5945
edder773 <st02045@naver.com>
2023-03-25 18:43:06 +0900

    upload

---
+++ Python/BOJ/15311 약 팔기(★).py
@@ -0,0 +1,11 @@
+import sys
+N = int(sys.stdin.readline())
+print(2000)
+print(str(1)*1000,str(1000)*1000)
+# result = []
+# for i in range(1000):
+#     result.append(1)
+# for j in range(1000):
+#     result.append(1000)
+# print(len(result))
+# print(*result)
